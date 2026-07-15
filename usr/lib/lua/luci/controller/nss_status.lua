00c865463cd0194d840862297f9a5b933636cd8e
MMCKBZn <1979161564@qq.com>
2026-07-15 18:39:26 +0800

    Delete usr/lib/lua/luci/controller directory

--- usr/lib/lua/luci/controller/nss_status.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.nss_status", package.seeall)
-function index()
-    entry({"admin", "nss_status"}, call("action_redirect"), _("NSS 状态"), 60).leaf = true
-end
-function action_redirect()
-    luci.http.redirect("/cgi-bin/nss_status")
-end
