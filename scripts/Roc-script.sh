005823f64dfa1a5c88e5efdce3e93821d3c53a1c
MMCKBZn <1979161564@qq.com>
2026-07-15 20:50:09 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -105,3 +105,7 @@ echo "baidu.com"  > package/luci-app-passwall/luci-app-passwall/root/usr/share/p
 
 ./scripts/feeds update -i -a
 ./scripts/feeds install -a
+
+if [ -f "$GITHUB_WORKSPACE/diy-part.sh" ]; then
+    bash "$GITHUB_WORKSPACE/scripts/diy-part.sh"
+fi
